6834c55d936ddf9b5812d0784f30d715e15402e7
Hamda7777 <hamdayusuf008@gmail.com>
2025-04-13 21:38:11 +0800

    adding first

---
+++ src/main/java/groupassignment/vendormanagement/VendorManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package groupassignment.vendormanagement;
+
+/**
+ *
+ * @author hamda
+ */
+public class VendorManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
